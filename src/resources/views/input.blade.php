ae23d0c899152447d751c453d1c581abfb496459
Chris Thompson <christhompsontldr@gmail.com>
2020-05-07 09:59:54 -0400

    Adding markdown

--- src/resources/views/input.blade.php
+++ src/resources/views/input.blade.php
@@ -195,11 +195,16 @@ if (isset($options['value'])) {
 }
 
 
+$fieldType = $type;
+
 //  switch field type to textarea for type=html
 switch ($type) {
     case 'html':
         $fieldType = 'textarea';
         $options['required'] = false;
+        break;
+    case 'markdown':
+        $fieldType = 'textarea';
         break;
     case 'datetime':
         $fieldType = 'text';
@@ -208,8 +213,7 @@ switch ($type) {
     case 'checkbox':
     case 'radio':
         $options['checked'] = isset($options['checked']) ? $options['checked'] : false;
-    default:
-        $fieldType = $type;
+        break;
 }
 
 
@@ -361,4 +365,24 @@ if (isset($options['class'])) {
         });
     </script>
     @endpushonce
+@endif
+
+@if ($type == 'markdown')
+    @pushonce('after-styles:markdown')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simplemde/1.11.2/simplemde.min.css" integrity="sha256-Is0XNfNX8KF/70J2nv8Qe6BWyiXrtFxKfJBHoDgNAEM=" crossorigin="anonymous" />
+    @endpushonce
+
+    @pushonce('after-scripts:markdown')
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/simplemde/1.11.2/simplemde.min.js" integrity="sha256-6sZs7OGP0Uzcl7UDsLaNsy1K0KTZx1+6yEVrRJMn2IM=" crossorigin="anonymous"></script>
+    @endpushonce
+    @push('after-scripts')
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var simplemde = new SimpleMDE({
+                element: document.getElementById("{{ $options['id'] }}"),
+	            toolbar: ["bold", "italic", "heading-3", "|", "quote", "|", "unordered-list", "ordered-list", "|", "link", "image"],
+            });
+        });
+    </script>
+    @endpush
 @endif
