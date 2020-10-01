6bd75143cdd32d12d1a6f2c676c3df9f5c999e2b
Chris Thompson <christhompsontldr@gmail.com>
2020-10-01 16:43:07 -0400

    Adding a workaround for readonly radios

--- resources/views/bs/radio.blade.php
+++ resources/views/bs/radio.blade.php
@@ -1,5 +1,15 @@
 @extends('form::bs.input')
 
 @section('input')
+{{-- https://stackoverflow.com/questions/155291/can-html-checkboxes-be-set-to-readonly --}}
+@if (Arr::get($options, 'readonly', false))
+    {{ Form::hidden($name, $value) }}
+    @php
+        $name = '_tmp_' . $name;
+
+        $options['disabled'] = true;
+    @endphp
+@endif
+
 {{ Form::radio($name, $value, $checked, $options) }}
 @overwrite
